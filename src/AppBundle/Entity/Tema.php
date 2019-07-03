0ebd84486b2f180700ef4eb1d21c9625eb9e39ba
walter1711 <walter_lencina32@outlook.com>
2019-07-03 08:27:59 -0300

    adicion de campo codigo a Tema

--- src/AppBundle/Entity/Tema.php
+++ src/AppBundle/Entity/Tema.php
@@ -18,11 +18,18 @@ class Tema
      * @ORM\Column(name="id", type="integer")
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
-     * @ORM\OneToOne(targetEntity="CaratulaAgregada", mappedBy="tema")
-     * @ORM\OneToOne(targetEntity="Expediente", mappedBy="tema")
+     * @ORM\OneToMany(targetEntity="CaratulaAgregada", mappedBy="tema")
+     * @ORM\OneToMany(targetEntity="Expediente", mappedBy="tema")
      */
     private $id;
-
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="codigo", type="string", length=255)
+     */
+    private $codigo;
+    
     /**
      * @var string
      *
@@ -63,5 +70,29 @@ class Tema
     public function getDescripcion()
     {
         return $this->descripcion;
+    }
+
+    /**
+     * Set codigo
+     *
+     * @param string $codigo
+     *
+     * @return Tema
+     */
+    public function setCodigo($codigo)
+    {
+        $this->codigo = $codigo;
+
+        return $this;
+    }
+
+    /**
+     * Get codigo
+     *
+     * @return string
+     */
+    public function getCodigo()
+    {
+        return $this->codigo;
     }
 }
