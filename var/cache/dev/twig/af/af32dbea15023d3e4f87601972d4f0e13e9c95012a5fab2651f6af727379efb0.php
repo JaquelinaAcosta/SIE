e4f68ea8973412ddd632b2df97403ef32b34dbc1
walter1711 <walter_lencina32@outlook.com>
2019-07-29 12:57:15 -0300

    Autocompletado funcionando en dependencia y expediente

--- var/cache/dev/twig/af/af32dbea15023d3e4f87601972d4f0e13e9c95012a5fab2651f6af727379efb0.php
+++ var/cache/dev/twig/af/af32dbea15023d3e4f87601972d4f0e13e9c95012a5fab2651f6af727379efb0.php
@@ -57,34 +57,34 @@ class __TwigTemplate_4604e30dd5d53019d7d6727eadb46743fca19a48092d189d2fec30b7c99
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "content"));
 
         // line 4
-        echo "    
+        echo "
     <style>
         input[type=text]{
             margin-bottom: 10px;
             padding:10px;            
-         }
-         input[type=submit]{
+        }
+        input[type=submit]{
             margin-bottom: 10px;
             padding:10px;            
-         }
+        }
 
     </style>
-    
+
     <div class=\"col-lg-10\" style=\"padding-left:  300px; padding-top:  50px\">
         <div class=\"list-group col-lg-11 col-md-offset-1\">
             <div class=\"expediente col-lg-10 panel panel-primary\"  style=\"padding: 10px\">
-                    <div class=\"panel-heading\" style=\"font-size:18px;\">
-                        Agregar Carátula para Expediente Nro.: ";
+                <div class=\"panel-heading\" style=\"font-size:18px;\">
+                    Agregar Carátula para Expediente Nro.: ";
         // line 21
         echo twig_escape_filter($this->env, $this->getAttribute(($context["expediente"] ?? $this->getContext($context, "expediente")), "nroExpediente", []), "html", null, true);
         echo "
-                    </div>
+                </div>
                 <br/>
-            ";
+                ";
         // line 24
         echo         $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->renderBlock(($context["form"] ?? $this->getContext($context, "form")), 'form_start', ["action" => "", "method" => "POST"]);
         echo "
-            <div class=\"col-lg-14\">
+                <div class=\"col-lg-14\">
                     <label class=\"text-default\">Tema:</label>
                     ";
         // line 27
@@ -112,21 +112,35 @@ class __TwigTemplate_4604e30dd5d53019d7d6727eadb46743fca19a48092d189d2fec30b7c99
         // line 34
         echo $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->searchAndRenderBlock($this->getAttribute(($context["form"] ?? $this->getContext($context, "form")), "fojas", []), 'widget');
         echo "
-                    
+
                     ";
         // line 36
         echo $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->searchAndRenderBlock($this->getAttribute(($context["form"] ?? $this->getContext($context, "form")), "Aceptar", []), 'widget');
         echo "
                     <a class=\"btn btn-default\" onclick=\"history.back()\"  role=\"button\">Volver</a>
-         
-            ";
+
+                    ";
         // line 39
         echo         $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->renderBlock(($context["form"] ?? $this->getContext($context, "form")), 'form_end');
         echo "
+                </div>
             </div>
         </div>
-    </div>
-";
+
+        <script>
+            \$('#caratulaagregada_tema').autocompleter({
+            url_list: \"";
+        // line 46
+        echo $this->env->getExtension('Symfony\Bridge\Twig\Extension\RoutingExtension')->getPath("tema_search");
+        echo "\",
+            url_get: \"";
+        // line 47
+        echo $this->env->getExtension('Symfony\Bridge\Twig\Extension\RoutingExtension')->getPath("tema_get");
+        echo "\"
+            });
+        </script>
+
+    ";
         
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
 
@@ -147,7 +161,7 @@ class __TwigTemplate_4604e30dd5d53019d7d6727eadb46743fca19a48092d189d2fec30b7c99
 
     public function getDebugInfo()
     {
-        return array (  124 => 39,  118 => 36,  113 => 34,  109 => 33,  104 => 31,  100 => 30,  95 => 28,  91 => 27,  85 => 24,  79 => 21,  60 => 4,  51 => 3,  29 => 1,);
+        return array (  138 => 47,  134 => 46,  124 => 39,  118 => 36,  113 => 34,  109 => 33,  104 => 31,  100 => 30,  95 => 28,  91 => 27,  85 => 24,  79 => 21,  60 => 4,  51 => 3,  29 => 1,);
     }
 
     /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
@@ -163,28 +177,28 @@ class __TwigTemplate_4604e30dd5d53019d7d6727eadb46743fca19a48092d189d2fec30b7c99
         return new Source("{% extends \"AppBundle::layout.html.twig\" %}
 
 {% block content %}
-    
+
     <style>
         input[type=text]{
             margin-bottom: 10px;
             padding:10px;            
-         }
-         input[type=submit]{
+        }
+        input[type=submit]{
             margin-bottom: 10px;
             padding:10px;            
-         }
+        }
 
     </style>
-    
+
     <div class=\"col-lg-10\" style=\"padding-left:  300px; padding-top:  50px\">
         <div class=\"list-group col-lg-11 col-md-offset-1\">
             <div class=\"expediente col-lg-10 panel panel-primary\"  style=\"padding: 10px\">
-                    <div class=\"panel-heading\" style=\"font-size:18px;\">
-                        Agregar Carátula para Expediente Nro.: {{ expediente.nroExpediente}}
-                    </div>
+                <div class=\"panel-heading\" style=\"font-size:18px;\">
+                    Agregar Carátula para Expediente Nro.: {{ expediente.nroExpediente}}
+                </div>
                 <br/>
-            {{form_start(form,{'action':'','method':'POST'})}}
-            <div class=\"col-lg-14\">
+                {{form_start(form,{'action':'','method':'POST'})}}
+                <div class=\"col-lg-14\">
                     <label class=\"text-default\">Tema:</label>
                     {{ form_errors(form.tema) }}
                     {{ form_widget(form.tema) }}
@@ -194,14 +208,22 @@ class __TwigTemplate_4604e30dd5d53019d7d6727eadb46743fca19a48092d189d2fec30b7c99
                     <label class=\"text-default\">Cantidad de Fojas:</label>
                     {{ form_errors(form.fojas) }}
                     {{ form_widget(form.fojas) }}
-                    
+
                     {{ form_widget(form.Aceptar) }}
                     <a class=\"btn btn-default\" onclick=\"history.back()\"  role=\"button\">Volver</a>
-         
-            {{form_end(form)}}
+
+                    {{form_end(form)}}
+                </div>
             </div>
         </div>
-    </div>
-{% endblock %}", "AppBundle:Expediente:caratulaAgregada.html.twig", "/var/www/html/gitSIE/src/AppBundle/Resources/views/Expediente/caratulaAgregada.html.twig");
+
+        <script>
+            \$('#caratulaagregada_tema').autocompleter({
+            url_list: \"{{ path('tema_search') }}\",
+            url_get: \"{{ path('tema_get') }}\"
+            });
+        </script>
+
+    {% endblock %}", "AppBundle:Expediente:caratulaAgregada.html.twig", "/var/www/html/SIE/src/AppBundle/Resources/views/Expediente/caratulaAgregada.html.twig");
     }
 }
