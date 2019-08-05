abf331d7d4d966baf9877772d22a31de37a9380b
Walter <walter_lencina32@outlook.com>
2019-08-04 21:32:03 -0300

    tipo de pase

--- src/AppBundle/Controller/MovimientoExpedienteController.php
+++ src/AppBundle/Controller/MovimientoExpedienteController.php
@@ -11,11 +11,96 @@ use AppBundle\Entity\Ubicacion;
 use AppBundle\Form\MovimientoExpedienteType;
 
 class MovimientoExpedienteController extends Controller {
-
+//
+//    /**
+//     * @Route("expediente/{id}/add/movimiento", name="nuevo_movimiento")
+//     */
+//    public function indexAction(Request $request, $id) {
+//        $em = $this->getDoctrine()->getEntityManager();
+//        $movimientoExpediente = new MovimientoExpediente();
+//        $ubicacionActual = null;
+//        $expediente = $em->getRepository("AppBundle:Expediente")->find($id);
+//        
+//        $form = $this->createForm(MovimientoExpedienteType::class, $movimientoExpediente);
+//        $form->handleRequest($request);
+//        
+//        if ($form->isSubmitted()) {
+//            $ubicacion_index = $form['ubicacion']->getData();
+//            $movimientoExpediente->setUsuario($this->getUser()->getIup());
+//             
+//            if($ubicacion_index == 1){
+//                 $mesaentrada = $em->getRepository("AppBundle:Dependencia")->find($form['mesaentrada']->getData()->getDependencia()->getId())->getMesaentrada();
+//                 $ubicacionActual = $mesaentrada;
+//                 $movimientoExpediente->setTipoSalida('Externa');
+//            }
+//            if($ubicacion_index == 2){
+//                $persona = $em->getRepository("AppBundle:Persona")->find($form['persona']['nombre']->getData()->getId());
+//                $ubicacionActual = $persona;
+//                $movimientoExpediente->setTipoSalida('Interna');
+//            }   
+//            if($ubicacion_index == 3){
+//                $lugarfisico = $em->getRepository("AppBundle:LugarFisico")->find($form['lugarfisico']['tipo']->getData()->getId());
+//                $ubicacionActual = $lugarfisico;
+//                $movimientoExpediente->setTipoSalida('Archivador');
+//            }               
+//            
+//            $movimientoExpediente->setExpediente($expediente);
+//            $movimientoExpediente->setFecha(date("d-m-Y H:i:s"));       
+//            $movimientoExpediente->setUbicacion($ubicacionActual);
+//
+//            
+//            $expediente->getMovimientos()->add($movimientoExpediente);            
+//            $expediente->setUbicacionActual($ubicacionActual);
+//            
+//            $em->persist($expediente);
+//            $em->flush();
+//            
+//           return $this->redirectToRoute('ver_expediente',['id'=>$id]);
+//        }
+//
+//        // replace this example code with whatever you need
+//        return $this->render('AppBundle:Expediente:movimiento.html.twig', [
+//                    'form' => $form->createView(),
+//                    'expediente' => $expediente
+//        ]);
+//    }
+//      
+//    /**
+//     * @Route("/ajax_movimiento", name="add_movimiento")
+//     */
+//    public function ajaxFormAction(Request $request) {
+//        $expediente = new Expediente();
+//
+//        $form = $this->createForm(MovimientoExpedienteType::class);
+//        $form->handleRequest($request);        
+//        
+//        return $this->render('AppBundle:Expediente:movimiento.html.twig', [
+//                    'form' => $form->createView(),
+//                    'expediente' => $expediente
+//        ]);
+//    }
+    
+    
     /**
-     * @Route("expediente/{id}/add/movimiento", name="nuevo_movimiento")
+     * @Route("/expediente/{id}/add/movimiento", name="elegir_movimiento")
      */
-    public function indexAction(Request $request, $id) {
+    public function indexMovimientoAction(Request $request,$id) {
+        $em = $this->getDoctrine()->getEntityManager();
+        $expediente = $em->getRepository("AppBundle:Expediente")->find($id);
+        
+        $form = $this->createForm(MovimientoExpedienteType::class);
+        $form->handleRequest($request);        
+        
+        return $this->render('AppBundle:Movimiento:chooser.html.twig', [
+                    'form' => $form->createView(),
+                    'expediente' => $expediente
+        ]);
+    }
+    
+    /**
+     * @Route("/expediente/{id}/add/movimiento/interno", name="movimiento_interno")
+     */
+    public function internoAction(Request $request,$id) {
         $em = $this->getDoctrine()->getEntityManager();
         $movimientoExpediente = new MovimientoExpediente();
         $ubicacionActual = null;
@@ -56,28 +141,6 @@ class MovimientoExpedienteController extends Controller {
             $em->flush();
             
            return $this->redirectToRoute('ver_expediente',['id'=>$id]);
-        }
-
-        // replace this example code with whatever you need
-        return $this->render('AppBundle:Expediente:movimiento.html.twig', [
-                    'form' => $form->createView(),
-                    'expediente' => $expediente
-        ]);
     }
-      
-    /**
-     * @Route("/ajax_movimiento", name="add_movimiento")
-     */
-    public function ajaxFormAction(Request $request) {
-        $expediente = new Expediente();
-
-        $form = $this->createForm(MovimientoExpedienteType::class);
-        $form->handleRequest($request);        
-        
-        return $this->render('AppBundle:Expediente:movimiento.html.twig', [
-                    'form' => $form->createView(),
-                    'expediente' => $expediente
-        ]);
-    }
-
+    
 }
