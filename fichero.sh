8ca3cb545b23b0387dcf1c49fe51446262061f55
Walter <walter_lencina32@outlook.com>
2019-09-11 09:07:19 -0300

    index.php añadido

---
+++ fichero.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd /var/www/html/blogvial/var
+
+sudo chmod -R 777 logs
+sudo chmod -R 777 cache
+sudo chmod -R 777 sessions
+
+sudo chown -R www-data:www-data logs
+sudo chown -R www-data:www-data cache
+sudo chown -R www-data:www-data sessions
